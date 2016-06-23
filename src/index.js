09340bc236d037654cc9d39fe54e0cfddaf0e7ee
rogerbf <roger@bergfalk.nu>
2016-06-23 11:57:15 +0200

    added scraper for sports section

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const news = require('./lib/news.js')
 const economy = require('./lib/economy.js')
 const culture = require('./lib/culture.js')
 const sthlm = require('./lib/sthlm.js')
+const sports = require('./lib/sports.js')
 
 module.exports = {
   news,
   economy,
   culture,
-  sthlm
+  sthlm,
+  sports
 }
---
+++ src/lib/sports.js
@@ -0,0 +1,50 @@
+const urls = require('./urls.js')
+const scrape = require('x-ray')({
+  filters: {
+    trim: function (value) {
+      return typeof value === 'string' ? value.trim() : value
+    }
+  }
+})
+
+const selectors = {
+  latest: '.article-teaser-list--timeline .widget__list-item--flex',
+  headlines: '.grid .section__content > a'
+}
+
+const scrapeLatest = scrape(urls.sport, selectors.latest, [{
+  datetime: 'time@datetime',
+  title: '.article-teaser__title-text',
+  summary: '.article-teaser__summary-text | trim',
+  url: 'a@href'
+}])
+
+const scrapeHeadlines = scrape(urls.sport, selectors.headlines, [{
+  datetime: 'time@datetime',
+  title: '.article-teaser__title-text',
+  summary: '.article-teaser__summary-text | trim',
+  url: '@href'
+}])
+
+const latest = () => {
+  return new Promise((resolve, reject) => {
+    scrapeLatest((err, latest) => {
+      if (err) reject(err)
+      resolve(latest)
+    })
+  })
+}
+
+const headlines = () => {
+  return new Promise((resolve, reject) => {
+    scrapeHeadlines((err, headlines) => {
+      if (err) reject(err)
+      resolve(headlines)
+    })
+  })
+}
+
+module.exports = {
+  latest,
+  headlines
+}
