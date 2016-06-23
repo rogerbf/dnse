a350c651fbd0f04964922cb1bc1610e9697bc4d6
rogerbf <roger@bergfalk.nu>
2016-06-23 11:31:16 +0200

    added scraper for economy section

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const news = require('./lib/news.js')
+const economy = require('./lib/economy.js')
 const culture = require('./lib/culture.js')
 
 module.exports = {
   news,
+  economy,
   culture
 }
